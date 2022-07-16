bac98f712e6c56bfe015a99ea2b7435428630abe
이원준 <80930788+rhdwn22c@users.noreply.github.com>
2022-07-16 16:25:11 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://i.com/" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["green", "blue"]
+colors = ["#000000", "blue"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이름")
-write("description", "설명")
-write("button", "버튼")
+write("name", "이원준")
+write("description", "대소고 2학년")
+write("button", "인스타그램")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "제목1": "내용1",
-  "제목2": "내용2",
-  "제목3": "내용3",
-  "제목4": "내용4"
+  "대소고 다니는 사람": "meronajoa0",
+  "인스타 아이디": "e_1jun_22_",
+  "대소고 찐따 공식": "so._.in",
+  "두카미 얼굴천재222222": "yr0118kim"
 }
 information(informations)
