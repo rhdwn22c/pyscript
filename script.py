74331de83480e3e82f04e05428ebfc93dcf108ad
이원준 <80930788+rhdwn22c@users.noreply.github.com>
2022-07-16 16:21:38 +0900

    Add files via upload

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ def write(name, desc):
 
 # 하단 버튼 링크 연결 함수
 def button(*args):
-  link = "https://instagram.com/" # https:// 꼭 붙여야 연결됩니다!
+  link = "https://www.naver.com/" # https:// 꼭 붙여야 연결됩니다!
   webbrowser.open(link)
 
 # 배경 색깔 설정 함수
@@ -21,19 +21,19 @@ def information(info):
     write("b" + str(i), info[key[i]])
 
 # 배경 색깔 설정
-colors = ["white", "black"]
+colors = ["green", "blue"]
 background(colors)
 
 # 이름과 설명, 버튼에 들어갈 글 설정
-write("name", "이원준")
-write("description", "Until make the best game of the world")
-write("button", "벝흔")
+write("name", "이름")
+write("description", "설명")
+write("button", "버튼")
 
 # 상세설명에 들어갈 제목과 글 설정
 informations = {
-  "좋아하는 것": "먹을거, 놀기",
-  "싫어하는 것": "푸팟퐁커리",
-  "취미": "사감 몰래 폰하기, 소설 보기",
-  "진로": "Game Developer"
+  "제목1": "내용1",
+  "제목2": "내용2",
+  "제목3": "내용3",
+  "제목4": "내용4"
 }
 information(informations)
